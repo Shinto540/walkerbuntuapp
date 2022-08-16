32d7dbdb4889c3e8e4e970ecafc5421d3b499381
jos-me <jos7.me@gmail.com>
2022-08-16 08:33:06 +0300

    ADD: product nav filter

--- components/home/PurchaseOnline.jsx
+++ components/home/PurchaseOnline.jsx
@@ -1,5 +1,6 @@
 import { ProductCard, ProductImage, ProductBody } from "./FeaturedProducts";
 import Styles from "../../styles/Product.module.css";
+import Link from "next/link";
 
 const PurchaseOnline = () => {
   return (
@@ -8,42 +9,67 @@ const PurchaseOnline = () => {
         <h2>Purchase Online on Hurst</h2>
         <span className={Styles.title__underline}></span>
       </div>
-
-      <div className="flex flex-wrap gap-10 mt-16">
-        <ProductCard>
-          <ProductImage url="/product/pr-1.png" />
-          <ProductBody title="dell - All in one" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-2.png" />
-          <ProductBody title="mac keyboard" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-3.png" />
-          <ProductBody title="headphone" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-4.png" />
-          <ProductBody title="laptop adaptor" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-5.png" />
-          <ProductBody title="dell - All in one" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-6.png" />
-          <ProductBody title="mac keyboard" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-7.png" />
-          <ProductBody title="headphone" />
-        </ProductCard>
-        <ProductCard>
-          <ProductImage url="/product/pr-8.png" />
-          <ProductBody title="laptop adaptor" />
-        </ProductCard>
-      </div>
+      <NavFilter />
+      <ProductDisplay />
     </section>
+  );
+};
+
+export const ProductDisplay = () => {
+  return (
+    <div className="grid grid-cols-4 gap-10 mt-16">
+      <ProductCard>
+        <ProductImage url="/product/pr-1.png" />
+        <ProductBody title="dell - All in one" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-2.png" />
+        <ProductBody title="mac keyboard" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-3.png" />
+        <ProductBody title="headphone" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-4.png" />
+        <ProductBody title="laptop adaptor" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-5.png" />
+        <ProductBody title="dell - All in one" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-6.png" />
+        <ProductBody title="mac keyboard" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-7.png" />
+        <ProductBody title="headphone" />
+      </ProductCard>
+      <ProductCard>
+        <ProductImage url="/product/pr-8.png" />
+        <ProductBody title="laptop adaptor" />
+      </ProductCard>
+    </div>
+  );
+};
+
+export const NavFilter = () => {
+  return (
+    <div className="flex justify-center gap-12 mt-7">
+      <Link href="">
+        <a className="font-medium text-gray-600 text-lg">New Arrivals</a>
+      </Link>
+      <Link href="">
+        <a className="font-medium text-gray-600 text-lg">Best Seller</a>
+      </Link>
+      <Link href="">
+        <a className="font-medium text-gray-600 text-lg">Most View</a>
+      </Link>
+      <Link href="">
+        <a className="font-medium text-gray-600 text-lg">Discounts</a>
+      </Link>
+    </div>
   );
 };
 
