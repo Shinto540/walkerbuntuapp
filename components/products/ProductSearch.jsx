0ba18e6aa8c7cc038c2c464ff9545b103b7760dc
Twinklefadhi <fadhilasief@gmail.com>
2022-08-19 17:00:37 +0300

    UPDATE:Returned normal

--- components/products/ProductSearch.jsx
+++ components/products/ProductSearch.jsx
@@ -1,15 +1,8 @@
-import { FaSearch } from "react-icons/fa";
+
 
 const ProductSearch = () => {
   return (
-    <main className="bg-gray-100 pb-16">
-      <section className="search">
-        <div className="flex top-50% px-10 py-10  h-[7vw] hover:text-blue-600 cursor-pointer">
-          <input type="search" placeholder="Search here.." />
-          <FaSearch className=""/>
-        </div>
-      </section>
-    </main>
+    <div>ProductSearch</div>
   );
 };
 
