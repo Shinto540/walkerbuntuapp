9bb57aabf5fd42b445d0263db15b12041b7a0266
Twinklefadhi <fadhilasief@gmail.com>
2022-08-19 13:04:10 +0300

    added product page on index,js

--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,10 +1,12 @@
 import Layout from "../../components/Layout";
 import PageHeader from "../../components/PageHeader";
+import ProductPage from "../../components/products/ProductPage";
 
 const Product = () => {
   return (
     <Layout>
       <PageHeader pageTitle="Product Grid View" />
+      <ProductPage />
     </Layout>
   );
 };
